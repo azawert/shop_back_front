7ced06fc200a4ffbf04e836fd68dd546b7615472
Igor <azawertik@gmail.com>
2022-12-30 21:53:24 +0300

    added

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,8 @@
 const Router = require('express')
 const brandController = require('../controllers/BrandController')
 const router = new Router()
-
-router.post('/',brandController.create)
+const auth = require('../middleware/authMiddleware')
+router.post('/',auth,brandController.create)
 router.get('/',brandController.getAll)
 router.get('/:id',brandController.getSingleBrand)
 
